8e6bf604be0bfbb36cd5541d4c3a5884fa378e61
Evens Antoine <evens7antoine@gmail.com>
2025-03-05 17:59:40 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   generateCard()
   console.log("Hello Rigo from the console!");
 };
-const generateCard = ()=> {
-  let allSuits = ["♦", "♣", "♥", "♠"];
+const generateCard = () => {
+  let allSuits = ["&spades;", "&hearts;", "&clubs;", "&diams;"]
   let allNumbers = [
     "2",
     "3",
@@ -28,11 +28,21 @@ const generateCard = ()=> {
   ];
   let randomSuit = Math.floor(Math.random() * allSuits.length)
   let randomNumber = Math.floor(Math.random() * allNumbers.length)
-  
+
   let number = allNumbers[randomNumber]
   let suit = allSuits[randomSuit]
   document.querySelector(".Top").innerHTML = suit
   document.querySelector(".Filling").innerHTML = number
   document.querySelector(".Bottom").innerHTML = suit
-  document.querySelector(" ").style.color =
+
+  const isRed =
+    suit === "&hearts;" || suit === "&diams;";
+    
+  document
+    .querySelectorAll(".Top, .Filling, .Bottom")
+    .forEach(element => {
+      element.style.color = isRed ? "red" : "";
+    });
+
+    document.querySelector("button").addEventListener("click", generateCard)
 }
